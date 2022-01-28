43a53f95a297cf5c23c5fd376bb17acf3f62bae7
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2022-01-28 18:49:59 +0200

    SOLID: Single Responsibility Principle
    Each class should have a single reason to change.

---
+++ Design Patterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntery("I learnt single resposibility today");
+            j.AddEntery("I practised some problem solving.");
+            Console.WriteLine(j);
+
+            var filename = @"c:\temp\myjournal.txt";
+            Persistence.SaveToFile(j, filename);
+            Console.WriteLine(Persistence.LoadFile(filename));
+        }
+    }
+}
---
+++ Design Patterns/Single_Responsibility_Principle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/*
+ * Definition: Every class should have a single reason to change. which means each class only
+ * has a single responsibility.
+ * As a byproduct you will get separation of concerns.
+ */
+namespace SOLID
+{
+    public class Journal
+    {
+        private readonly List<string> enteries = new List<string>();
+
+        public int AddEntery(string entery)
+        {
+            enteries.Add(entery);
+            return enteries.Count;
+        }
+        public void RemoveEnteryAt(int index)
+        {
+            enteries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return String.Join(Environment.NewLine, enteries);
+        }
+    }
+    public static class Persistence
+    {
+        public static void SaveToFile(Journal j, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, j.ToString());
+            }
+        }
+        public static string LoadFile(string fileName)
+        {
+            return File.ReadAllText(fileName);
+        }
+    }
+}
