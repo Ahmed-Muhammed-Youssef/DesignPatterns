3b6796066569457de510f13658e47208df25970b
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2022-11-04 15:51:36 +0200

    Bridge Design Pattern
    a structural design pattern, which decouples an interface (hierarchy)
    from an implementation (hierarchy). pervents the cartesin product
    of objects.

---
+++ 7_Bridge/BridgeExample.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _7_Bridge
+{
+    public class BridgeExample
+    {
+        public static void Test() 
+        {
+            var circle = new Circle (new WindowsRenderer(), 5);
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+
+        }
+    }
+    internal interface IRenderer 
+    {
+        void RenderCircle(float radius);
+    }
+    internal class AndroidRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            System.Console.WriteLine($"Drawing a circle on android platform of radius {radius}");
+        }
+    }
+    internal class WindowsRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            System.Console.WriteLine($"Drawing a circle on windows platform of radius {radius}");
+        }
+    }
+    internal abstract class Shape 
+    {
+        protected readonly IRenderer renderer;
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName: nameof(renderer));
+        }
+        public abstract void Draw ();
+        public abstract void Resize(float factor);
+    }
+    internal class Circle : Shape
+    {
+        private float radius;
+        public Circle (IRenderer renderer, float radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+}
---
+++ 7_Bridge/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_Bridge
+{
+/* 
+* Category: Structural Pattern
+* Definition: 
+* A mechanism that decouples an interface (hierarchy) from an implementation (hierarchy). 
+* used sometimes with crossplatform application as the implementation differs depending on 
+* the os running the application.
+*/
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            // BridgeExample.Test();
+        }
+    }
+}
