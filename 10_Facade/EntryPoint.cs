a2d1ba9c576f89c250865b57a6468b48325dc4dd
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2022-11-10 12:01:30 +0200

    Facade Design Pattern
    Structural Design Patternm which is used to provide a simple interface
    over sophisticated one or multiple subsystems / classes.

---
+++ 10_Facade/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10_Facade
+{
+/* 
+* Category: Structural Pattern
+* Definition: 
+* Used to provide a simple, easy to understand user interface over a large and sophisticated body of code. 
+*/
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
